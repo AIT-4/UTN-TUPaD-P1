8c1e0a38bc1caa23a839b85d9fc93220ecf1b9b1
Federico Durussel <federicodurussel@outlook.com>
2025-04-11 22:45:57 -0300

    Se simplifica código de ejercicio n°10

--- 03 Estructuras Condicionales/Ejercicios/ejercicio10.py
+++ 03 Estructuras Condicionales/Ejercicios/ejercicio10.py
@@ -7,6 +7,9 @@ print ("ESTACIONES DEL AÑO")
 
 # Pedimos el hemisferio para el primer condicional
 hemisferio = input("Ingrese el hemisferio en el que se encuentra. N para norte y S para sur: ")
+hemisferio = hemisferio.lower()
+
+print(hemisferio)
 
 # Solicitamos el dia del mes
 dia = int(input("Ingrese el DIA del mes: "))
@@ -21,22 +24,22 @@ mes = int(input("Ingrese el MES del año: "))
 # Convertimos la respuesta en un valor mas facil de operar
 
 if (mes == 12 and dia <= 21) or mes == 1 or mes == 2 or (mes == 3 and dia <= 20):
-    if hemisferio == "N" or hemisferio == "n":
+    if hemisferio == "n":
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Invierno")
     else:
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Verano")
 elif (mes == 3 and dia >= 21) or mes == 4 or mes == 5 or (mes == 6 and dia <= 20):
-    if hemisferio == "N" or hemisferio == "n":
+    if hemisferio == "n":
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Primavera")
     else:
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Otoño")
 elif (mes == 6 and dia >= 21) or mes == 7 or mes == 8 or (mes == 9 and dia <= 20):
-    if hemisferio == "N" or hemisferio == "n":
+    if hemisferio == "n":
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Verano")
     else:
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Invierno")
 else:
-    if hemisferio == "N" or hemisferio == "n":
+    if hemisferio == "n":
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Otoño")
     else:
         print(f"Por encontrarse el {dia} del {mes} la estacion del año es Primavera")
